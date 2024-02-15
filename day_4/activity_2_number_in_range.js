d7025fcf86823d25f9721632e037b8087ebb452a
Ariston <aristoncatipay123@gmail.com>
2024-02-15 13:47:22 +0800

    Activity 2: Print number in range.

---
+++ day_4/activity_2_number_in_range.js
@@ -0,0 +1,7 @@
+function numberInRange(startingNumber, endingNumber){
+    for (let counter = startingNumber; counter <= endingNumber; counter++){
+        console.log(counter);
+    }
+}
+
+numberInRange(3, 15);

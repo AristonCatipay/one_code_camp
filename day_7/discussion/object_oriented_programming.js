2d827aa9dc3d27edc6bbd164c6b7eefdf3b6ce76
AristonCatipay <aristoncatipay123@gmail.com>
2024-02-21 12:00:12 +0800

    Notes for Object Oriented Programming in Javascript.

---
+++ day_7/discussion/object_oriented_programming.js
@@ -0,0 +1,22 @@
+// Object Oriented Programming
+// Object-oriented programming (OOP) is defined as a programming paradigm (and not a specific language) built on the concept of objects, 
+// i.e., a set of data contained in fields, and code, indicating procedures – instead of the usual logic-based system.
+
+// Properties
+// color : Brown
+// size : Medium
+// state : Open
+// madeOf : Metal
+
+// Method - Uses
+// storage
+// shelter
+// pad
+
+// OOP Implementation
+// Object
+// Class
+// Inheritance
+// Polymorphism
+// Abstraction
+// Encapsulation

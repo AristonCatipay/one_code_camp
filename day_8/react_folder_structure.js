0b57874e81e5d4dddaa567303fd87fdb450afc1c
AristonCatipay <aristoncatipay123@gmail.com>
2024-02-22 16:43:45 +0800

    Create notes for React folder structure.

---
+++ day_8/react_folder_structure.js
@@ -0,0 +1,33 @@
+// Dependencies & packages & versions & scripts
+// 1. package-lock.json
+// 2. package.json
+
+// public/index.html
+// In the entire react application you will be updating a single index.html file.
+// React is a Single Page Application(SPA)
+
+// public/index.html
+// <div id=root></div>
+// This is the single div we will add the changes.
+
+// src/ 
+// Contains the javascript and css files.
+
+// src/App.js
+// This links the div root container to the rest of the file directory.
+// Traffic Enforcer - it will decide the content.
+// For small website you can add the contents here.
+
+// node_modules
+// Default libraries is the "react" and "react-dom"
+// "react" - handle the logic
+// "react-dom" - intermediary between the react app and the virtual dom.
+
+// import App from './App';
+// When we import a file we can store it in a variable and use that.
+
+
+
+
+
+

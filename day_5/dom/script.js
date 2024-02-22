29991a45690c8f12cf7f2d96204dcbaf0c85fb3a
AristonCatipay <aristoncatipay123@gmail.com>
2024-02-22 14:02:26 +0800

    Create the frontend project initial files.

--- day_5/dom/script.js
+++ day_5/dom/script.js
@@ -1,4 +1,10 @@
-// Target HTML Elements
+// DOM - Document Object Model
+//       Object that represents the page you see in the web browser.
+//       and provides you with an API to interact with it.
+//       Web Browser constructs the DOM when it loads an HTML document.
+//       and structures all the elements in a tree-like representation.
+//       Javascript can access the DOM to dynamically change the content
+//       structure, and style a web page.
 
 // Get element by ID
 var headingElement = document.getElementById('heading');

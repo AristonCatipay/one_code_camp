024ba76975314c8083e33905b7a732a6ad7f3f8d
AristonCatipay <aristoncatipay123@gmail.com>
2024-03-04 16:54:25 +0800

    Connect to the MongoDB database using Mongoose.

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,37 @@
-// Require Express
+// // Require Express
+// const express = require("express");
+
+// // Initialize the application and Middleware
+// const app = express();
+
+// // Listen to the port
+// app.listen(3000, () => {
+//   console.log("Listening...");
+// });
+
+// // Routes
+// // GET request handler
+// app.get("/books", (request, response) => {
+//   response.json({ message: "Welcome to the API" });
+// });
+
 const express = require("express");
-
-// Initialize the application and Middleware
 const app = express();
+const mongoose = require("mongoose");
+const port = 3000;
+mongoose
+  .connect(
+    "mongodb+srv://aristoncatipay123:1MPiUM4dGgETJDWh@cluster0.vamlj4g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Listening port ${port} and connected to MongoDB`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
 
-// Listen to the port
-app.listen(3000, () => {
-  console.log("Listening...");
-});
-
-// Routes
-// GET request handler
 app.get("/books", (request, response) => {
   response.json({ message: "Welcome to the API" });
 });
